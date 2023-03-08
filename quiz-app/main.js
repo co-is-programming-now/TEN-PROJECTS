e0637efc10016810abfd5bd08551baddf9300d22
co-is-programming-now <constanzadesigner@gmail.com>
2023-03-08 16:21:25 -0300

    the ten projects folder

---
+++ quiz-app/main.js
@@ -0,0 +1,21 @@
+const quizData = [
+    {
+        question: 'How old ir Constanza?',
+        a: '10',
+        b: '61',
+        c: '19',
+        d: '20',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2023?',
+        a: 'Javascrpit',
+        b: 'C+',
+        c: 'SQL',
+        d: 'Java',
+        correct: 'Javascript'
+    }, {
+        question: 'ddd',
+    }
+
+
+]
